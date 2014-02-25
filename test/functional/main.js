ee7a877da28a00a5cf2330f12601abc44f52d6fa
Ghislain Seguin <ghislain@gmail.com>
2014-02-25 13:25:10 -0700

    Added functional test

---
+++ test/functional/main.js
@@ -0,0 +1,4 @@
+define( [ "msgfmt!test/nls/resources" ], function( i18n ){
+	document.getElementById( "fixture" ).innerHTML =
+		i18n[ "found results" ]({ NUM_RESULTS: "2",NUM_CATEGORIES: "2" });
+});
